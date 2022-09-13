c592cf9b5c378e8be1b6837d4169d413953271a5
stecarraa <stefano@caratozzolo.me>
2022-09-13 16:36:13 +0200

    feat: seeding ok

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
+    //
+    public function index(){
+        $comics = Comic::all();
+        return view('comics.index',compact ('comics'));
+    }
     //
 }
