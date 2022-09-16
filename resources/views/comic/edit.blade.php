47619ca790ce1e511c0303040de875291e394a93
stecarraa <stefano@caratozzolo.me>
2022-09-16 16:13:01 +0200

    feat: add old() medthod to edit.blade

--- resources/views/comic/edit.blade.php
+++ resources/views/comic/edit.blade.php
@@ -21,28 +21,28 @@
             <div class="form-row">
               <div class="form-group col-md-6">
                 <label for="title">Title</label>
-                    <input name="title" type="text" class="form-control" id="title" placeholder="Enter title" value="{{$comic->title}}">
+                    <input name="title" type="text" class="form-control" id="title" placeholder="Enter title" value="{{old('title', $comic->title)}}">
               </div>
               <div class="form-group col-md-6">
                 <label for="title">Series</label>
-                    <input name="series" type="text" class="form-control" id="title" placeholder="Enter title" value="{{$comic->series}}">
+                    <input name="series" type="text" class="form-control" id="title" placeholder="Enter title" value="{{old('series', $comic->series)}}">
               </div>
               <div class="form-group col-md-6">
                 <label for="description">Description</label>
-                <textarea name="description" id="description" cols="30" rows="5" class="form-control">value="{{$comic->description}}"</textarea>
+                <textarea name="description" id="description" cols="30" rows="5" class="form-control" >{{old('description', $comic->description)}}</textarea>
               </div>
             </div>
             <div class="form-group">
                 <label for="price">Price</label>
-                <input name="price" type="text" class="form-control" id="price"  placeholder="dilli-ding dilli-dong" value="{{$comic->price}}">
+                <input name="price" type="text" class="form-control" id="price"  placeholder="dilli-ding dilli-dong" value="{{old('price', $comic->price)}}">
             </div>
             <div class="form-group">
                 <label for="price">Image Url</label>
-                 <input name="thumb" type="text" class="form-control" id="imgUrl"  placeholder="Incolla url" value="{{$comic->thumb}}">
+                 <input name="thumb" type="text" class="form-control" id="imgUrl"  placeholder="Incolla url" value="{{old('thumb', $comic->thumb)}}">
             </div>
             <div class="form-group">
                 <label for="sale_date">Sale Date</label>
-                <input name="sale_date" type="date" class="form-control" id="date" value="{{$comic->sale_date}}">
+                <input name="sale_date" type="date" class="form-control" id="date" value="{{old('sale_date', $comic->sale_date)}}">
             </div>
             <div class="form-row">
               <div class="form-group col-md-4">
